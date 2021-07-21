0fe22b756c0aa68d89b7f64b4da77229c6ea2cfd
ZQ <ziqiao@cs.unc.edu>
2021-07-21 05:20:09 -0400

    Update stp_tocnf.sh

--- process/script/stp_tocnf.sh
+++ process/script/stp_tocnf.sh
@@ -1,4 +1,4 @@
-/playpen/ziqiao/2project/stp-cm/build/stp -r --disable-simplifications --output-CNF $1
+${STP} -r --disable-simplifications --output-CNF $1
 cp name_cnf.txt $1.cnf
 cat output_0.cnf >> $1.cnf
 sh $BOOM/script/simp.sh $1.cnf 1 1 
